b77c74bcaf916ddc4596df6dfbfedfe09020cd0d
tct123 <42028373+tct123@users.noreply.github.com>
2025-03-16 13:42:50 +0100

    Update app.py

--- src/togatoastapp/app.py
+++ src/togatoastapp/app.py
@@ -16,7 +16,7 @@ class TogaToastApp(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box(children=[toga.Button(text="Trigger toastmessage", on_click=lambda _:self.show_toast(message="Hello World"))])
+        main_box = toga.Box(children=[toga.Button(text="Trigger toastmessage", on_press=lambda _:show_toast(self=self,message="Hello World"))])
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
